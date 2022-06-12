df97851b48bd535ae10bebd6cadc60ab8d5873c7
HermannKateu <sontiahermann3@gmail.com>
2022-06-12 22:23:11 +0100

    Comppletion

--- holiday-project/src/components/NumberInput.vue
+++ holiday-project/src/components/NumberInput.vue
@@ -4,11 +4,11 @@
             Number of days
         </label>
         <input type="number" class="outline-none my-1 h-12 pl-4 text-black-400 border border-gray-200 bg-gray-100 
-            rounded-md" min="1" placeholder="18" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" readonly>
+            rounded-md" min="1" placeholder="18"  :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" readonly>
     </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
 defineProps({
     modelValue: {
         type: String,
