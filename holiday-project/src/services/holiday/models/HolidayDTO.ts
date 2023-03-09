f4250b01872bf1ebf796f0b5b41539128bc500ce
Sontia Kateu Junior Herman <sontia.kateu@lao-sarl.cm>
2023-03-09 19:05:04 +0100

    inserting the openapi in the project

---
+++ holiday-project/src/services/holiday/models/HolidayDTO.ts
@@ -0,0 +1,35 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export type HolidayDTO = {
+    /**
+     * identifier the holiday
+     */
+    id?: string;
+    /**
+     * the startingDate of the holiday
+     */
+    startingDate?: string;
+    /**
+     * the endingDate of the holiday
+     */
+    endingDate?: string;
+    /**
+     * the code of the speciality
+     */
+    returnDate?: string;
+    /**
+     * the description of the holiday
+     */
+    description?: string;
+    /**
+     * the number of days of the holiday
+     */
+    numberOfDays?: number;
+    /**
+     * the date of the holiday creation
+     */
+    createdAt?: string;
+};
+
